cf9ed3e752d723586dac82ae012d877fd70e4127
Ryan Wu <raynors233@gmail.com>
2020-05-19 11:59:01 +0800

    Refactor JSON entities to fix parsing error

--- lib/model/comment_entity.dart
+++ lib/model/comment_entity.dart
@@ -1,40 +1,39 @@
+import 'dart:core';
+
 class CommentEntity {
-  String by;
-  int id;
-  List<int> kids;
-  int parent;
-  String text;
-  int time;
-  String type;
+    String by;
+    int id;
+    List<int> kids;
+    int parent;
+    String text;
+    int time;
+    String type;
 
-  CommentEntity(
-      {this.by,
-        this.id,
-        this.kids,
-        this.parent,
-        this.text,
-        this.time,
-        this.type});
+    CommentEntity({this.by, this.id, this.kids, this.parent, this.text, this.time, this.type});
 
-  CommentEntity.fromJson(Map<String, dynamic> json) {
-    by = json['by'];
-    id = json['id'];
-    kids = json['kids'].cast<int>();
-    parent = json['parent'];
-    text = json['text'];
-    time = json['time'];
-    type = json['type'];
-  }
+    factory CommentEntity.fromJson(Map<String, dynamic> json) {
+        return CommentEntity(
+            by: json['by'],
+            id: json['id'],
+            kids: json['kids'] != null ? new List<int>.from(json['kids']) : null,
+            parent: json['parent'],
+            text: json['text'],
+            time: json['time'],
+            type: json['type'],
+        );
+    }
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['by'] = this.by;
-    data['id'] = this.id;
-    data['kids'] = this.kids;
-    data['parent'] = this.parent;
-    data['text'] = this.text;
-    data['time'] = this.time;
-    data['type'] = this.type;
-    return data;
-  }
+    Map<String, dynamic> toJson() {
+        final Map<String, dynamic> data = new Map<String, dynamic>();
+        data['by'] = this.by;
+        data['id'] = this.id;
+        data['parent'] = this.parent;
+        data['text'] = this.text;
+        data['time'] = this.time;
+        data['type'] = this.type;
+        if (this.kids != null) {
+            data['kids'] = this.kids;
+        }
+        return data;
+    }
 }
