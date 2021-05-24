00c47fb273d50bde47ed67477f7a765e357f9d03
Ryan Wu <raynors233@gmail.com>
2021-05-24 15:12:17 +0800

    Update dependencities

--- lib/repo/hacker_news_api.dart
+++ lib/repo/hacker_news_api.dart
@@ -1,14 +1,15 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:yaha/model/comment_entity.dart';
 import 'package:yaha/model/story_entity.dart';
 
 class HackerNewsApi {
-  final String _baseUrl = "https://hacker-news.firebaseio.com/";
+  static final String _baseUrl = "https://hacker-news.firebaseio.com/";
 
   Future<List<int>> getTopStories() async {
-    var response = await http.get(_baseUrl + 'v0/topstories.json');
+    var response = await http.get(Uri.parse(_baseUrl + 'v0/topstories.json'));
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
 
@@ -20,7 +21,8 @@ class HackerNewsApi {
   }
 
   Future<StoryEntity> getStory(int id) async {
-    var response = await http.get(_baseUrl + "v0/item/$id.json");
+    debugPrint("getting story id=$id");
+    var response = await getItem(id);
     if (response.statusCode == 200) {
       return StoryEntity.fromJson(jsonDecode(response.body));
     } else
@@ -28,10 +30,13 @@ class HackerNewsApi {
   }
 
   Future<CommentEntity> getComment(int id) async {
-    var response = await http.get(_baseUrl + "v0/item/$id.json");
+    debugPrint("getting comment id=$id");
+    var response = await getItem(id);
     if (response.statusCode == 200) {
       return CommentEntity.fromJson(jsonDecode(response.body));
     } else
       throw Exception("Failed to load comment");
   }
+
+  Future<http.Response> getItem(int id) => http.get(Uri.parse(_baseUrl + "v0/item/$id.json"));
 }
