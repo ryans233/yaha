dac9164c24da2e06616bc5a327aa0940f34110fd
Ryan Wu <raynors233@gmail.com>
2021-05-24 16:09:42 +0800

    Refactor logic of loading comments for better performance

--- lib/page/comments_page.dart
+++ lib/page/comments_page.dart
@@ -1,13 +1,14 @@
 import 'dart:async';
-import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:html_unescape/html_unescape.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:intl/intl.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:yaha/model/comment_entity.dart';
 import 'package:yaha/model/story_entity.dart';
 import 'package:yaha/repo/hacker_news_api.dart';
+import 'package:yaha/util/url_utils.dart';
 
 class CommentsPage extends StatefulWidget {
   CommentsPage({Key key, @required this.storyEntity}) : super(key: key);
@@ -18,145 +19,179 @@ class CommentsPage extends StatefulWidget {
 }
 
 class _CommentsPageState extends State<CommentsPage> {
-  var api = HackerNewsApi();
+  final _api = HackerNewsApi();
+  final dateFormatter = DateFormat("yyyy-MM-dd HH:mm:ss");
+  final _comments = Map<int, CommentEntity>();
+  final _depth = Map<int, int>();
+  final Map<int, List<int>> _tempOrdering = Map();
+  final List<int> _orderedComments = List.empty(growable: true);
+  final List<int> _hiddenComments = List.empty(growable: true);
+
   var _isLoading = false;
-  var cachedComments = Map<int, List<CommentEntity>>();
-  var htmlUnecapse = HtmlUnescape();
-  var fetchingList = Map<int, bool>();
 
   @override
   void initState() {
     super.initState();
-    loadComments(widget.storyEntity.id, widget.storyEntity.kids);
+    loadComments(widget.storyEntity.id);
   }
 
   @override
   void dispose() {
     super.dispose();
-    fetchingList.clear();
-    cachedComments.clear();
+    _comments.clear();
   }
 
   @override
   Widget build(BuildContext context) {
     if (_isLoading)
-      return Center(child: CupertinoActivityIndicator());
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    else if (_comments.isEmpty)
+      return Center(
+        child: Text("No comment"),
+      );
     else
-      return _buildCommentsListView(widget.storyEntity.id, true);
+      return _buildComments();
   }
 
-  Future<void> loadComments(int parentId, List<int> commentIds) async {
+  Future<void> loadComments(int parentId) async {
+    debugPrint("loadComments parentId=$parentId");
     setState(() {
       _isLoading = true;
     });
-
-    fetchComments(parentId, commentIds)
-        .then((value) => cachedComments[parentId] = value)
-        .catchError((e) {
-      setState(() {
-        _isLoading = false;
-        print("error: $e");
-        cachedComments.remove(parentId);
-      });
-    }).whenComplete(() {
+    loadCommentWithChildren(1, widget.storyEntity.id, widget.storyEntity.kids)
+        .listen((event) {
+      print(event);
+    }, onDone: () {
+      print("tt onDone");
+      _orderComments();
+      debugPrint("depth=" + _depth.toString());
+      debugPrint("_orderedComments=" + _orderedComments.toString());
+      debugPrint("_comments=" + _comments.toString());
       setState(() {
         _isLoading = false;
       });
     });
   }
 
-  Future<List<CommentEntity>> fetchComments(
-      int parentId, List<int> commentIds) async {
-    if (cachedComments.containsKey(parentId) &&
-        cachedComments[parentId] != null &&
-        cachedComments[parentId].isNotEmpty)
-      return Future.value(cachedComments[parentId]);
-    else
-      return Future.wait(commentIds.map((e) => api.getComment(e)));
+  Stream loadCommentWithChildren(int depth, int parentId, List<int> ids) {
+    if (ids != null)
+      _tempOrdering
+          .putIfAbsent(parentId, () => List.empty(growable: true))
+          .addAll(ids);
+    return ids == null
+        ? Stream.empty()
+        : Stream.fromIterable(ids)
+            .flatMap((value) => _api.getComment(value).asStream())
+            .doOnData((event) {
+              debugPrint(
+                  "_api.getComment(value).asStream()).doOnData((event) = $event");
+              if (event.id == 27204890) print("here");
+              _comments[event.id] = event;
+              _depth[event.id] = depth;
+            })
+            .flatMap((event) =>
+                loadCommentWithChildren(depth + 1, event.id, event.kids))
+            .doOnDone(() {
+              debugPrint("onDone");
+            });
   }
 
-  Widget _buildCommentsListView(int parentId, bool isFirstLevel) {
-    return ListView.builder(
-        shrinkWrap: !isFirstLevel,
-        physics: isFirstLevel
-            ? AlwaysScrollableScrollPhysics()
-            : NeverScrollableScrollPhysics(),
-        itemBuilder: (context, index) =>
-            _buildCommentItem(cachedComments[parentId][index]),
-        itemCount: cachedComments[parentId].length);
-  }
+  Widget _buildComments() => ListView.builder(
+        itemBuilder: (context, index) => _buildCommentItem(index),
+        itemCount: _orderedComments.length,
+      );
 
-  Widget _buildCommentItem(CommentEntity comment) {
-    return Container(
-        margin: EdgeInsets.only(top: 10.0, left: 10.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (comment.deleted != null && comment.deleted)
-              Text(
-                "**Deleted**",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              )
-            else
-              Text.rich(
-                TextSpan(children: [
-                  TextSpan(
-                      text: comment.by, style: TextStyle(color: Colors.blue)),
-                  TextSpan(text: ": "),
-                  TextSpan(text: htmlUnecapse.convert(comment.text)),
-                ]),
-              ),
-            if (comment.kids != null)
-              if (fetchingList[comment.id] == null &&
-                  cachedComments[comment.id] == null)
-                Center(
-                  child: GestureDetector(
-                    child: Padding(
-                      child: Text(
-                        "Load more",
-                        style: TextStyle(fontWeight: FontWeight.bold),
+  Widget _buildCommentItem(int index) {
+    debugPrint("building ${_orderedComments[index]}");
+    final comment = _comments[_orderedComments[index]];
+    return _hiddenComments.contains(comment.id)
+        ? SizedBox.shrink()
+        : Container(
+            margin: EdgeInsets.only(
+              left: (_depth[_orderedComments[index]] - 1) * 12.0 + 8,
+              right: 8.0,
+              top: 2,
+              bottom: 2,
+            ),
+            child: Card(
+              child: Container(
+                padding: EdgeInsets.all(8),
+                child: comment.deleted == true
+                    ? Text("***Deleted***")
+                    : Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "${comment.by} ${dateFormatter.format(DateTime.fromMillisecondsSinceEpoch(comment.time * 1000))}:",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Html(
+                            data: comment.text,
+                            onLinkTap: (url, context, attributes, element) =>
+                                launchUrl(url),
+                          ),
+                          // Text(
+                          //   comment.text
+                          // ),
+                          comment.kids != null
+                              ? Container(
+                                  margin: EdgeInsets.all(4),
+                                  child: Row(
+                                    children: [
+                                      Switch(
+                                        value: !comment.kids.any((element) =>
+                                            _hiddenComments.contains(element)),
+                                        onChanged: (show) =>
+                                            toggleCommentVisibility(
+                                                comment.id, show),
+                                      ),
+                                      Text("Show/Hide"),
+                                    ],
+                                  ),
+                                )
+                              : SizedBox.shrink(),
+                        ],
                       ),
-                      padding: EdgeInsets.only(top: 10),
-                    ),
-                    onTap: () {
-                      setState(() {
-                        fetchingList[comment.id] = true;
-                      });
-                    },
-                  ),
-                )
-              else if (cachedComments[comment.id] != null)
-                _buildCommentsListView(comment.id, false)
-              else
-                FutureBuilder(
-                  future: fetchComments(comment.id, comment.kids),
-                  builder: (context, snapshot) {
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.waiting:
-                        return Center(child: Padding(
-                          child: CupertinoActivityIndicator(),
-                          padding: EdgeInsets.only(top: 10),
-                        ),);
-                        break;
-                      case ConnectionState.done:
-                        if (snapshot.hasError) return Text("error");
-                        if (snapshot.hasData) {
-                          fetchingList.remove(comment.id);
-                          List<CommentEntity> list = snapshot.data;
-                          cachedComments[comment.id] = list;
-                          return _buildCommentsListView(comment.id, false);
-                        } else
-                          return Container();
-                        break;
-                      default:
-                        return Container();
-                        break;
-                    }
-                  },
-                )
-          ],
-        ));
+              ),
+            ),
+          );
+  }
+
+  toggleCommentVisibility(int id, bool show) {
+    setState(() {
+      _toggleCommentVisibility(id, show);
+    });
+  }
+
+  _toggleCommentVisibility(int id, bool show) {
+    debugPrint("id=$id show=$show");
+    var kids = _comments[id].kids;
+    if (kids != null)
+      kids.forEach((e) {
+        show ? _hiddenComments.remove(e) : _hiddenComments.add(e);
+        return _toggleCommentVisibility(e, show);
+      });
+  }
+
+  _orderComments() {
+    _orderedComments.clear();
+    var list = _tempOrdering.entries.toList();
+    list.sort((a, b) => a.key.compareTo(b.key));
+    _orderedComments.addAll(list.fold<List<int>>(List.empty(growable: true),
+        (previousValue, element) {
+      if (element.key == widget.storyEntity.id)
+        previousValue.addAll(element.value);
+      else
+        previousValue.insertAll(
+            previousValue.indexOf(element.key) + 1, element.value);
+      return previousValue;
+    }));
   }
 }
