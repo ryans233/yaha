8298596f9db7bc5e2501e81e4d18faf608b264e7
Ryan Wu <raynors233@gmail.com>
2021-05-25 18:00:59 +0800

    Implement floating app bar on stories page

--- lib/page/stories_page.dart
+++ lib/page/stories_page.dart
@@ -26,118 +26,125 @@ class _StoriesPageState extends State<StoriesPage> {
             context.select((StoriesModel value) => value.currentType);
 
         return Scaffold(
-            appBar: AppBar(
-              title: DropdownButton(
-                onChanged: (value) {
-                  context.read<StoriesModel>().setType(value);
-                  keyRefreshIndicator.currentState.show();
-                },
-                value: currentType,
-                underline: SizedBox.shrink(),
-                icon: SizedBox.shrink(),
-                items: StoryType.values
-                    .map(
-                      (e) => DropdownMenuItem<StoryType>(
-                        child: Text(
-                          e.toString(),
-                          style: TextStyle(color: Colors.black),
-                        ),
-                        value: e,
-                      ),
-                    )
-                    .toList(),
-              ),
-              actions: _buildActions(context),
-            ),
             body: RefreshIndicator(
-              key: keyRefreshIndicator,
-              onRefresh: () => context.read<StoriesModel>().getNewStories(),
-              child: ListView.builder(
-                physics: AlwaysScrollableScrollPhysics(),
-                controller: context.watch<StoriesModel>().scrollController,
-                itemCount: cachedStories.length + 1,
-                itemBuilder: (context, index) {
-                  if (cachedStories.isEmpty)
-                    return Container(
-                      child: Center(
-                        child: Column(
-                          children: [
-                            Icon(
-                              Icons.accessibility,
-                              size: 64,
-                            ),
-                            Text(
-                              "Pull to refresh",
-                              style: TextStyle(fontSize: 24),
-                            ),
-                          ],
+          key: keyRefreshIndicator,
+          onRefresh: () => context.read<StoriesModel>().getNewStories(),
+          child: CustomScrollView(
+            physics: AlwaysScrollableScrollPhysics(),
+            controller: context.watch<StoriesModel>().scrollController,
+            slivers: [
+              SliverAppBar(
+                flexibleSpace: GestureDetector(
+                  onDoubleTap: () =>
+                      context.read<StoriesModel>().scrollController.jumpTo(0),
+                ),
+                floating: true,
+                title: DropdownButton(
+                  onChanged: (value) async {
+                    context.read<StoriesModel>().setType(value);
+                    await keyRefreshIndicator.currentState.show();
+                    context.read<StoriesModel>().scrollController.jumpTo(0);
+                  },
+                  value: currentType,
+                  underline: SizedBox.shrink(),
+                  icon: SizedBox.shrink(),
+                  items: StoryType.values
+                      .map(
+                        (e) => DropdownMenuItem<StoryType>(
+                          child: Text(
+                            e.toString(),
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          value: e,
                         ),
-                      ),
-                      margin: EdgeInsets.symmetric(vertical: 100),
-                    );
-                  else if (index < cachedStories.length) {
-                    return buildStoryListItem(cachedStories[index]);
-                  } else if (isLoadingMore) {
-                    return Container(
-                      height: 50.0,
-                      child: Center(
-                        child: CircularProgressIndicator(),
-                      ),
-                    );
-                  } else
-                    return SizedBox.shrink();
-                },
+                      )
+                      .toList(),
+                ),
+                actions: _buildActions(context),
               ),
-            ));
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    if (cachedStories.isEmpty)
+                      return Container(
+                        child: Center(
+                          child: Column(
+                            children: [
+                              Icon(
+                                Icons.accessibility,
+                                size: 64,
+                              ),
+                              Text(
+                                "Pull to refresh",
+                                style: TextStyle(fontSize: 24),
+                              ),
+                            ],
+                          ),
+                        ),
+                        margin: EdgeInsets.symmetric(vertical: 100),
+                      );
+                    else if (index < cachedStories.length) {
+                      return buildStoryListItem(cachedStories[index]);
+                    } else if (isLoadingMore) {
+                      return Container(
+                        height: 50.0,
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      );
+                    } else
+                      return SizedBox.shrink();
+                  },
+                  childCount: cachedStories.length + 1,
+                ),
+              ),
+            ],
+          ),
+        ));
       },
     );
   }
 
   Widget buildStoryListItem(StoryEntity item) {
     return Card(
-      child: InkWell(
-        child: Hero(
-          child: Material(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+      child: GestureDetector(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                item.title,
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Row(
                 children: [
                   Text(
-                    item.title,
+                    item.by.toString(),
                     style: TextStyle(
-                      fontSize: 16,
+                      fontSize: 14,
                     ),
                   ),
-                  SizedBox(
-                    height: 4,
+                  Spacer(),
+                  Text(
+                    (item.descendants == null || item.descendants == 0)
+                        ? ""
+                        : item.descendants.toString(),
+                    textAlign: TextAlign.end,
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
-                  Row(
-                    children: [
-                      Text(
-                        item.by.toString(),
-                        style: TextStyle(
-                          fontSize: 14,
-                        ),
-                      ),
-                      Spacer(),
-                      Text(
-                        (item.descendants == null || item.descendants == 0)
-                            ? ""
-                            : item.descendants.toString(),
-                        textAlign: TextAlign.end,
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
-                  )
                 ],
-              ),
-            ),
+              )
+            ],
           ),
-          tag: item.id,
         ),
         onTap: () => Navigator.pushNamed(context, '/story', arguments: item),
       ),
@@ -154,5 +161,4 @@ class _StoriesPageState extends State<StoriesPage> {
       ),
     ];
   }
-
 }
